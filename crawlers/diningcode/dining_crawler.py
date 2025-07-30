155c29f4ec78741931ed31f000236a734d0e53ca
박준현 <gktkwk91@naver.com>
2025-07-30 23:44:40 +0900

    feat: dining_crawler 해당 음식점까지 접근 성공

--- crawlers/diningcode/dining_crawler.py
+++ crawlers/diningcode/dining_crawler.py
@@ -1,4 +1,5 @@
 from crawlers.base_crawler import BaseCrawler
+from selenium.webdriver.common.by import By
 import os
 
 
@@ -9,45 +10,50 @@ class DiningCrawler(BaseCrawler):
         super().__init__()
         self.category = 'dining'
         self.base_news_url = self.get_category_urls(self.category)
-        self.press_url_list = self.get_press_urls()
 
-    def get_press_urls(self):
-        news_press_urls = []
 
-        urls = self.base_news_url
-        soup = self.get_soup(url=urls)
-
-        press_wrap = soup.find('div', 'pressWrap')
-        press_list = press_wrap.find('ul', 'pressList')
-        press_block_list_li = press_list.find_all('li', 'block')
-
-        for press_block_li in press_block_list_li:
-            press_li_list = press_block_li.find_all('li')
-            for press_li in press_li_list:
-                press = {}
-                press_a = press_li.find('a')
-                # print(f'a pressName {press_a.text} href = {press_a['href']}')
-
-                """
-                name : 언론사 이름,
-                url : 언론사 기사 목록
-
-                위 딕션널리를 news_press_urls 리스트로 담음
-                """
-                sub_str = '/news/'
-                press['name'] = press_a.text if press_a else None
-                press['url'] = self.base_news_url+press_a['href'][len(sub_str)::] if press_a['href'] else None
-                news_press_urls.append(press)
-
-        print(news_press_urls)  
-
-        return news_press_urls
-           
-    
     def crawl(self):
-        return super().crawl()
+        search_element = self.get_first_element(
+            url=self.base_news_url,
+            by=By.CSS_SELECTOR,
+            css_selector='#root > header > div > div > div:nth-of-type(2) > div.Input__Wrap > input.Search__Input'
+        )
+        search_element.send_keys('오레노라멘')
+
+        print(search_element.tag_name)
+
+        click_element = self.get_element(
+            by=By.CSS_SELECTOR,
+            css_selector='#root > header > div > div > div:nth-of-type(2) > div.Input__Wrap > button.search'
+        )
+        if click_element.is_displayed():
+            click_element.click()
+            restaurant_list_element = self.get_element(
+                    by=By.CSS_SELECTOR,
+                    css_selector='#root > div.content > div.Scroll__List__Section > div.Poi__List__Wrap'
+                )
+            
+            restaurant_url = self.get_element(
+                    by=By.CSS_SELECTOR,
+                    css_selector='#blockwJpwvXzEFYVj'
+                ).get_attribute('href')
+
+            # 아직 리뷰까지 못가져옴 확인해야함.
+            restaurant_info_elements = self.get_first_element(
+                url=restaurant_url,
+                by=By.ID,
+                css_selector='#div_review'
+            )
+            print(restaurant_url)
+            input()
 
 
-# news = DiningCrawler()
 
-# news.crawl()
+        
+        
+
+
+
+dining = DiningCrawler()
+
+dining.crawl()
